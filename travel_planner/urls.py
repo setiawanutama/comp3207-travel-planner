96bd49fe20bec8865774dda5a33f80c60c43db77
Setiawan Utama <setiawanutama@gmail.com>
2018-01-05 20:04:29 +0000

    Removed allauth pattern in urls.py

--- travel_planner/urls.py
+++ travel_planner/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	url(r'^accounts/', include('allauth.urls')),
+	# url(r'^accounts/', include('allauth.urls')),
 	url(r'^$', views.index, name='index'),
 	
 	url(r'^trips/', include('trips.urls')),
